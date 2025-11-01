8a95e08d09210af90e73d0e430b3bbd6ad000929
= <=>
2025-11-01 15:03:48 +0530

    user therad

--- src/Multithreading/MyThread.java
+++ src/Multithreading/MyThread.java
@@ -4,44 +4,27 @@ import javax.sound.midi.SysexMessage;
 
 public class MyThread extends Thread{
 
-    public MyThread(String name){
-        super(name); // calling Thread class constuctor
-    }
+
     @Override
     public void run() {
-        for (int  i =0; i<5; i++){
-            System.out.println(Thread.currentThread().getName() + " is running ");
-            //Thread.yield();
+        int i =0;
+        while (i<20){
+            System.out.println("Hello world");
+            i++;
         }
     }
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread("thread akansha");
-        MyThread t2 = new MyThread("thread ayush");
-        t1.start();
-        t2.start();
+        MyThread myThread = new MyThread();
+
+        myThread.start();
+        System.out.println(Thread.currentThread().getState());
+        myThread.join();
+        System.out.println(myThread.getState());
+        System.out.println(Thread.currentThread().getState());
     }
+    //Daemon tHREAD : runs in background, JVM does not wait fot them
+    // JVM just sees main method is completed and all user threads are completed, it gets terminated
 
-    // without yield : random result
-//    thread ayush is running
-//    thread ayush is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread akansha is running
-//    thread akansha is running
 
-    // with yield() method
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
-//    thread akansha is running
-//    thread ayush is running
+
 }
