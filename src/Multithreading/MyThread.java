dec192257c2183fa6db5b8bf5ae62c57537014ba
= <=>
2025-11-01 13:46:44 +0530

    setPriority method

--- src/Multithreading/MyThread.java
+++ src/Multithreading/MyThread.java
@@ -3,52 +3,31 @@ package Multithreading;
 import javax.sound.midi.SysexMessage;
 
 public class MyThread extends Thread{
+
+    public MyThread(String name){
+        super(name); // calling Thread class constuctor
+    }
     @Override
     public void run() {
-//        for (int i = 1; i<=5; i++){
-//            try {
-//                Thread.sleep(1000);// current thread execution suspended for specified period of time
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            System.out.println(i);
-//        }
-        try {
-            Thread.sleep(5000);// current thread execution suspended for specified period of time
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for(int i =0; i<5; i++){
+            System.out.println(Thread.currentThread().getName() +
+                    " - Priority : " + Thread.currentThread().getPriority() + " - count " + i);
+            try{
+                Thread.sleep(100);
+            }catch (Exception e){
+
+            }
         }
     }
-
     public static void main(String[] args) throws InterruptedException {
-//        MyThread t1 = new MyThread();
-//        t1.start();
-//        System.out.println(t1.getState());
-//        System.out.println("hello");
-//        System.out.println(Thread.currentThread().getState());
-//        System.out.println(t1.getState()); // TIMED_WAITING or RUNNABLE
-//        RUNNABLE
-//        hello
-//        RUNNABLE
-//        TIMED_WAITING
-
-//        RUNNABLE
-//        hello
-//        RUNNABLE
-//        RUNNABLE
-
-        MyThread t1 = new MyThread();
+        MyThread t1 = new MyThread("thread akansha");
+        MyThread t2 = new MyThread("thread ayush");
+        MyThread t3 = new MyThread("thread honey");
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t3.setPriority(Thread.MAX_PRIORITY);
         t1.start();
-        System.out.println(t1.getState());
-        System.out.println("hello");
-        t1.join();
-        System.out.println(Thread.currentThread().getState());
-        System.out.println(t1.getState());
-
-//        RUNNABLE
-//        hello
-//        RUNNABLE
-//        TERMINATED
-
+        t2.start();
+        t3.start();
     }
 }
