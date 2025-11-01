dc4bea4d07543033eea14673a377e8a044ff6e27
= <=>
2025-11-01 12:13:13 +0530

    class having extended class already then need to implement the Runnable

---
+++ src/Multithreading/A.java
@@ -0,0 +1,19 @@
+package Multithreading;
+
+public class A extends B implements Runnable{
+    @Override
+    public void run() {
+
+    }
+
+    // class cannot extend multiple classses
+    // public class A extends B, Thread
+
+    // we can use implements
+
+
+
+
+
+
+}
