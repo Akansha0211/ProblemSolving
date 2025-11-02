59ab1bd217d2484a67ea37f52f2226f3e8a1a947
= <=>
2025-11-02 07:31:33 +0530

    making block synchronized

--- src/Multithreading/Counter.java
+++ src/Multithreading/Counter.java
@@ -4,8 +4,15 @@ public class Counter {
 
     private int count = 0;
 
-    public synchronized void increment(){
-        count++;
+    public void increment(){
+        // synchronized block
+        synchronized (this){
+            // this : one instance , multiple threads accessing it , only one thread can access it
+            // multple objects of Counter, indp
+
+            // method / Block : Synchronized
+            count++;
+        }
     }
     public int getCount(){
         return count;
