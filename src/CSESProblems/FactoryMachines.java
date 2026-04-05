4ef050c91305743fa0ee3d254b1f86af05ef1ce0
unknown <Akansha0211>
2026-04-05 23:54:30 +0530

    Brute force approach for FactoryMachines

--- src/CSESProblems/FactoryMachines.java
+++ src/CSESProblems/FactoryMachines.java
@@ -1,71 +1,55 @@
-package CSESProblems.Maths;
+//package CSESProblems;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.InputMismatchException;
 
 /*
-* https://cses.fi/problemset/task/1164/
+* https://cses.fi/problemset/task/1620/
 * */
-public class RoomAllocation {
-
-    // Brute Force approach : Sorted on the basis of Departure time,
-    // create a array of room number allocated to customers --> size n
-    // pick the initial customer and allocate a room Number to it
-    // then try to give this roomNumber (to the cstomers whose arrival > dept of this intial one )
-    // after allocating this room Number to the customer --> update the start , end time
-    // do this for every customer ( which is not allocated to any room)
-
+public class FactoryMachines {
 
     public static void main(String[] args) {
         FastIO io = new FastIO();
         int n = io.nextInt();
-        int[][] customers = new int[n][2];
-
-        for(int i =0; i<n; i++){
-            int arrival = io.nextInt();
-            int dept = io.nextInt();
-
-            int[] customer = new int[3];
-            customer[0] = arrival;
-            customer[1] = dept;
-            customer[2] = i; // index of current person
-            customers[i] = customer;
+        int t = io.nextInt();
+        int[] machines = new int[n];
+        for(int i=0; i<n; i++){
+            machines[i] = io.nextInt();
         }
 
-        //sorting on the basis of dept time
-        Arrays.sort(customers, (a, b) -> a[1] - b[1] );
-        int[] roomNumbersAllocated = new int[n];
-        int ans = 0;
-        int start = 0;
-        int end = 0;
+        // Brute Force approach...
+        /*
+        * eg n = 3, t = 7,  machines ==> [3 2 5]
+        * at t =2  --> only 1 product by 2nd machine
+        * at t = 3 --> 1 product by 1st machine
+        * at t = 4 --> 2nd product by 2nd machine( the one taking 2 seconds to make a product)
+        * at t = 5 --> 1 product by 3rd machine
+        * at t = 6 --> By now we got 2 products by 1 machine, 3 products by 2nd machine , 1 by 3rd machine
+        * at t = 7
+        * at t = 8 --> Its giving 2 products by 1st machine, 4 products by 2nd machine , 1 product by 3rd machine ==> in all its --> 7 products
+        *
+        * so if notice we are getting more in the multiples of 2 ( ie multiples of min ( minimum time taken by machine))
+        * This is linear --> we can do the same with Binary search in oprimised approach
+        * */
 
-        for(int i = 0; i<n; i++){
-            int[] customer = customers[i];
-            if(roomNumbersAllocated[customer[2]] ==0){
-                // no room allocated
-                ans++;
-                start = customer[0];
-                end = customer[1];
-                roomNumbersAllocated[customer[2]] = ans;
+        int min = Integer.MAX_VALUE;
+        for(int mach : machines)min = Math.min(min, mach);
+
+
+        int val = 1;
+        int time = 0;
+        while(true){
+            time = min*val;
+            int products = 0;
+            for(int mach : machines){
+                products += (time/mach);
             }
-            for(int j = i+1; j<n; j++){
-                // if not allocated , can we allocate the same room Number as ith customer
-                int[] customer_j  = customers[j];
-                int index = customer_j[2];
-                int arrival = customer_j[0];
-                int depart = customer_j[1];
-                if(roomNumbersAllocated[index] == 0 && arrival > end){
-                    roomNumbersAllocated[index] = ans;
-                    start = arrival;
-                    end = depart;
-                }
+            if(products >= t){
+                io.println(time);
+                break;
             }
+            val++;
         }
-        io.println(ans); // gives the number of rooms required
-        StringBuilder str = new StringBuilder();
-        for (int allocation : roomNumbersAllocated) { str.append(allocation).append(" "); }
-        io.println(str);
         io.close();
     }
 
